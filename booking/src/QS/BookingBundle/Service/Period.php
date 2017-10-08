edc21464d1edbce2112f0326bc73c20dd7c38685
Souhail Moussaoui <smsouhail@gmail.com>
2017-10-08 13:52:10 +0200

    UPDATE Period Service to get unavailable periods for an event

--- booking/src/QS/BookingBundle/Service/Period.php
+++ booking/src/QS/BookingBundle/Service/Period.php
@@ -2,8 +2,57 @@
 
 namespace QS\BookingBundle\Service;
 
+use Doctrine\ORM\EntityManager;
 use QS\BookingBundle\Entity\Event;
+use QS\BookingBundle\Entity\EventPeriod;
 
 class Period
 {
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Get all unavailabale Period for an event
+     *
+     * @param Event $event
+     *
+     * @return Array
+     */
+    public function getUnavailabilityForEvent(Event $event)
+    {
+        $periods = [];
+
+        $qb = $this->em->createQueryBuilder()
+            ->addSelect('o.eventDate fullDate')
+            ->from('QSBookingBundle:Order', 'o')
+            ->andWhere('o.event = :event')
+            ->addGroupBy('o.eventDate')
+            ->andHaving('SUM(o.qtyResv) >= :maxResvDay')
+                ->setParameters([
+                    'event' => $event,
+                    'maxResvDay' => $event->getMaxResvDay(),
+                ])
+        ;
+
+        $periods = array_merge($periods, $qb->getQuery()->getScalarResult());
+
+        $qb = $this->em->createQueryBuilder()
+            ->addSelect('p')
+            ->from('QSBookingBundle:Period', 'p')
+            ->leftJoin('p.events', 'ep', 'WITH', 'ep.event = :event')
+            ->andWhere('ep.action = :action')
+                ->setParameters([
+                    'event' => $event,
+                    'action' => EventPeriod::ACTION_EXCLUDE,
+                ])
+        ;
+
+        $periods = array_merge($periods, $qb->getQuery()->getScalarResult());
+
+        return $periods;
+    }
 }
