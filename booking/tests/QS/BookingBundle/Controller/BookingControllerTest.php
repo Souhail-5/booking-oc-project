d6ff6a039c4cd2e5897ab72d6fe10ee8dd567896
Souhail Moussaoui <smsouhail@gmail.com>
2017-10-19 13:27:53 +0200

    UPDATE tests

--- booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
+++ booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
@@ -11,6 +11,16 @@ class BookingControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/billetterie/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Commander vos billets', $crawler->filter('#event-wrap a')->text());
+        $this->assertContains('Visite du Musée du Louvre', $crawler->filter('#event-wrap')->text());
+    }
+
+    public function testGuichet()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billetterie/guichet/visite-musee-louvre');
+        $form = $crawler->selectButton('Valider mon choix')->form();
+        $form['qs_bookingbundle_order[email]'] = 'fauxemail';
+        $client->submit($form);
+        $this->assertContains('form-errors', $client->getResponse()->getContent());
     }
 }
