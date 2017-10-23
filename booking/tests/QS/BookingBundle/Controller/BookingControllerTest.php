35b2c85ff284aaecf6edeabdd94ccbec0551db21
Souhail Moussaoui <smsouhail@gmail.com>
2017-10-23 15:37:20 +0200

    UPDATE Functional Tests

--- booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
+++ booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
@@ -9,12 +9,20 @@ class BookingControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/billetterie/');
+        $crawler = $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Visite du Musée du Louvre', $crawler->filter('#event-wrap')->text());
+        $this->assertContains('Accueil', $crawler->filter('head title')->text());
     }
 
-    public function testGuichet()
+    public function testBilletterie()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billetterie/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Billetterie', $crawler->filter('head title')->text());
+    }
+
+    public function testGuichetInvalidEmail()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/billetterie/guichet/visite-musee-louvre');
@@ -22,5 +30,13 @@ class BookingControllerTest extends WebTestCase
         $form['qs_bookingbundle_order[email]'] = 'fauxemail';
         $client->submit($form);
         $this->assertContains('form-errors', $client->getResponse()->getContent());
+    }
+
+    public function testConfirmationInvalidOrder()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billetterie/confirmation/x');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertContains('Oups', $client->getResponse()->getContent());
     }
 }
