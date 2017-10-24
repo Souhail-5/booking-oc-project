a68e906e7dc78ce42cbfcf5925f9a36c6bf99e91
Souhail Moussaoui <smsouhail@gmail.com>
2017-10-24 15:40:10 +0200

    UPDATE move booking related tests to BookingBundle folder

--- booking/src/QS/BookingBundle/Tests/Controller/BookingControllerTest.php
+++ booking/src/QS/BookingBundle/Tests/Controller/BookingControllerTest.php
@@ -4,14 +4,23 @@ namespace QS\BookingBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class BookingControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testGuichetInvalidEmail()
     {
         $client = static::createClient();
+        $crawler = $client->request('GET', '/billetterie/guichet/visite-musee-louvre');
+        $form = $crawler->selectButton('Valider mon choix')->form();
+        $form['qs_bookingbundle_order[email]'] = 'fauxemail';
+        $client->submit($form);
+        $this->assertContains('form-errors', $client->getResponse()->getContent());
+    }
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    public function testConfirmationInvalidOrder()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billetterie/confirmation/x');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertContains('Oups', $client->getResponse()->getContent());
     }
 }
--- booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
+++ booking/tests/QS/BookingBundle/Controller/BookingControllerTest.php
@@ -21,22 +21,4 @@ class BookingControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Billetterie', $crawler->filter('head title')->text());
     }
-
-    public function testGuichetInvalidEmail()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/billetterie/guichet/visite-musee-louvre');
-        $form = $crawler->selectButton('Valider mon choix')->form();
-        $form['qs_bookingbundle_order[email]'] = 'fauxemail';
-        $client->submit($form);
-        $this->assertContains('form-errors', $client->getResponse()->getContent());
-    }
-
-    public function testConfirmationInvalidOrder()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/billetterie/confirmation/x');
-        $this->assertEquals(404, $client->getResponse()->getStatusCode());
-        $this->assertContains('Oups', $client->getResponse()->getContent());
-    }
 }
