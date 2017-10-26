ab61f681e651c68ef80f0dc4439f5896ea083d03
Souhail Moussaoui <smsouhail@gmail.com>
2017-10-26 16:37:32 +0200

    UPDATE prod.sh

--- utils/prod.sh
+++ utils/prod.sh
@@ -3,6 +3,8 @@ sudo chown -R webapp:webapp var/cache
 sudo chown -R webapp:webapp var/logs
 sudo rm -rf var/cache/*
 sudo rm -rf var/logs/*
+find var/ -type d -exec sudo chmod 777 {} \;
+find var/ -type f -exec sudo chmod 777 {} \;
 
 echo "Update parameters.yml"
 sudo vim app/config/parameters.yml
